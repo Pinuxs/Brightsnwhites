4217c1b4af49e4afa7a4c4637216549c193f305e
Pinuxs <104815657+Pinuxs@users.noreply.github.com>
2024-04-25 13:42:23 +1000

    formatting php

--- send_email.php
+++ send_email.php
@@ -3,12 +3,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
-    echo '<script>console.log("Welcome to GeeksforGeeks!!"); </script>'; 
+
+    echo '<script>console.log("Test"); </script>'; 
+
     $to = "Hayden_hg@outlook.com"; // Specify your email address here
     $subject = "Contact Form Submission";
     $body = "Name: $name\nEmail: $email\n\n$message";
-    echo '<script>console.log("Welcome to GeeksforGeeks!"); </script>'; 
+
+    echo '<script>console.log("Test2"); </script>'; 
+
     header("Location: http://haydens-macbook-air.local/Brightsnwhites/index.html");
+
     if (mail($to, $subject, $body)) {
         echo "Email sent successfully!";
         header("Location: http://haydens-macbook-air.local/Brightsnwhites/index.html");
